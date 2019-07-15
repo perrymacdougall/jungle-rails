0dc28b60a0143a027f2de3f9d745f922a967a725
Perry <perrymacdougall@gmail.com>
2019-07-15 22:46:50 +0000

    Added a class method to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email)
+    # If the user exists AND the password entered is correct.
+    if user && user.authenticate(password)
+      # Save the user id inside the browser cookie. This is how we keep the user
+      # logged in when they navigate around our website.
+      return user
+    end
+  end
+
 end
