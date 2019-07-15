335cda6ef872a0b4b55760c45985289a415699e3
Perry <perrymacdougall@gmail.com>
2019-07-15 00:13:05 +0000

    added ability to restrict and delete ratings based on login

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,35 @@
+class ReviewsController < ApplicationController
+
+  def create
+    @product = Product.find params[:product_id]
+    @review = @product.reviews.new(review_params)
+    @review.user_id = current_user.id
+
+    if @review.save
+      redirect_to [:products]
+    else
+      redirect_to :back
+    end
+  end
+
+  def destroy
+    @review = Review.find params[:id]
+
+    if @review.destroy
+      redirect_to [:products]
+    else
+      redirect_to :back
+    end
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :description,
+      :rating
+    )
+  end
+
+end
